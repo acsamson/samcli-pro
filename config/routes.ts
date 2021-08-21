adeb7025992ace5e6a432b773bcf229453518081
Lian <lianshanchun@baidu.com>
2021-08-21 15:03:39 +0800

    feat: beta1.0.0

---
+++ config/routes.ts
@@ -0,0 +1,43 @@
+﻿export default [
+  {
+    path: '/user',
+    layout: false,
+    routes: [
+      {
+        path: '/user',
+        routes: [
+          {
+            name: 'login',
+            path: '/user/login',
+            component: './user/Login',
+          },
+        ],
+      },
+      {
+        component: './404',
+      },
+    ],
+  },
+  {
+    path: '/demo',
+    name: 'demo',
+    icon: 'crown',
+    access: 'canAdmin',
+    routes: [
+      { path: '/demo', redirect: '/demo/list' },
+      {
+        path: '/demo/list',
+        name: 'list.table-list',
+        icon: 'table',
+        component: './TableList',
+      },
+    ],
+  },
+  {
+    path: '/',
+    redirect: '/demo',
+  },
+  {
+    component: './404',
+  },
+];
---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,26 @@
+import { useIntl } from 'umi';
+import { DefaultFooter } from '@ant-design/pro-layout';
+
+export default () => {
+  const intl = useIntl();
+  const defaultMessage = intl.formatMessage({
+    id: 'app.copyright.produced',
+    defaultMessage: 'AC模板',
+  });
+
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <DefaultFooter
+      copyright={`${currentYear} ${defaultMessage}`}
+      links={[
+        {
+          key: 'ACWeb',
+          title: 'AC模板',
+          href: '/',
+          blankTarget: true,
+        },
+      ]}
+    />
+  );
+};
